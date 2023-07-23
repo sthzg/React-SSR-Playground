91f8e72340c514d727871e7800d237adfcd0c723
Stephan Herzog <sthzgvie@gmail.com>
2023-07-23 18:02:32 +0200

    feat(*): add favicon

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import './assets/favicon.ico';
 import './index.css';
 
 ReactDOM.hydrateRoot(
